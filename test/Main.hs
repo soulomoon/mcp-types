977fe42d29a4f5a4c61966a1d9d5d7681bd322a8
soulomoon <patrickwalesss@gmail.com>
2025-05-22 00:04:51 +0800

    Add .vscode to .gitignore, create mcp-gen.cabal, and implement initial tests for mcp-gen

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
 import Test.Tasty (defaultMain, testGroup)
+import System.Process (readProcess)
+import Test.Tasty.HUnit (testCase, (@?=))
+import Data.List (isInfixOf)
 
 main :: IO ()
-main = defaultMain $ testGroup "All Tests" [ ]
+main = defaultMain $ testGroup "All Tests"
+  [ testCase "mcp-gen runs and outputs expected text" $ do
+      output <- readProcess "mcp-gen" [] ""
+      let expected = "Finished generating Haskell data types"
+      -- Check that the output contains the expected substring
+      (expected `isInfixOf` output) @?= True
+  ]
